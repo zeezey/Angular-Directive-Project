8683f8f9965392ee32a073b5eb53b14b0eabdcba
Arun Selvaraj <zeezey@Aruns-MacBook-Pro.local>
2015-09-16 14:52:09 -0600

    tw

--- app/app.js
+++ app/app.js
@@ -1,5 +1,21 @@
-var app = angular.module('directivePractice', []);
+var app = angular.module('directivePractice', ['ngRoute']);
 
-app.config(function(){
+app.config(function($routeProvider) {
+  $routeProvider
+//  route for home page
+  .when('/', {
+    templateUrl: 'template.html',
+    controller: 'mainController'
+    
+  });
+    
+//app.controller('mainController', function ($scope, $route, $routeParams, $location) {
+//    $scope.$route = $route;
+//     $scope.$location = $location;
+//     $scope.$routeParams = $routeParams;
+//    $scope.message = 'Everyone come and see how good I look!';
+//
+//});
 
 });
+
